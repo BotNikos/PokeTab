4a3b4b2d5e0fabfb25114694b1ecfb38a4672dde
BotNikos <boss.nikitos1212@mail.ru>
2025-06-30 08:58:42 +0300

    Add initial database creation to makefile

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-poke-tab: poke-tab.lisp
+poke-tab: poke-tab.lisp custom-tab.db
 	sbcl --eval "(asdf:make :poke-tab/executable)"
 
 install: poke-tab
@@ -6,6 +6,9 @@ install: poke-tab
 	mkdir -p /usr/local/bin/poke-tab/
 	cp ./poke-tab /usr/local/bin/poke-tab
 	cp -r ./resources/ /usr/local/bin/poke-tab/
+
+custom-tab.db:
+	sqlite3 custom-tab.db < database.sql
 
 uninstall:
 	systemctl stop poke-tab
--- database.sql
+++ database.sql
@@ -28,29 +28,3 @@ create table history (
 	   query blob,
 	   time text default (datetime ('now', 'localtime'))
 );
-
-insert into categories (title, color) values
-('Games', '#8BE9FD'),
-('Work', '#FFB86C'),
-('Books', '#50FA7B');
-
-insert into items (title, url, category_id) values
--- Games
-('StopGame', 'https://stopgame.ru', 1),
-('DFT', 'https://dtf.ru/games', 1),
-('Metacritic', 'https://metacritic.com', 1),
-('Psprices', 'https://psprices.com/region-us/index', 1),
-('Eshop usa', 'https://www.nintendo.com/us/store/', 1),
--- Work
-('192.168.4.16', 'http://192.168.4.16', 2),
-('192.168.4.116', 'http://192.168.4.116', 2),
-('GitLab', 'http://172.16.80.2/', 2),
-('YouTrack', 'http://172.16.80.1/issues', 2),
--- Books
-('zLibrary', 'https://z-lib.id/', 3);
-
-insert into query_types (title) values
-('Create category'),
-('Create item'),
-('Goto item'),
-('Search');
